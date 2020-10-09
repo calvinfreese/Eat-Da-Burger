d09c6d98004315f06382d6bc95edf0ca022098ec
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-08 21:22:54 -0500

    changed const to let for expHandlebars

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,23 @@
-const connection = require('../config/connection');
+let connection = require('../config/connection');
 
 
 let orm = {
-    all: function(tableName, cb) {
+    selectAll: function(tableName, cb) {
         let queryString = `SELECT * FROM ${tableName};`;
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function(tableName, val, cb) {
+        let queryString = `INSERT INTO ${tableName} (burger_name) VALUES (${val});`;
+        connection.query(queryString, function(err, result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    updateOne: function(tableName, colVal, condition, cb) {
+        let queryString = `UPDATE ${tableName} SET ${JSON.stringifiy(colVal)} WHERE ${condition};`;
         connection.query(queryString, function(err, result){
             if (err) throw err;
             cb(result);
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express');
-const expHandleBars = require('express-handlebars');
-const routes = require('./controllers/burgerController.js');
+let expHandleBars = require('express-handlebars');
+let routes = require('./controllers/burgerController.js');
 
 let PORT = process.env.PORT || 8080;
 let app = express();
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(routes);
+
 
 app.engine('handlebars', expHandleBars({defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
 
+app.use(routes);
 
 app.listen(PORT, function(){
     console.log("Server listening on http://localhost:" + PORT);
