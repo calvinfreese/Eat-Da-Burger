c43562c95ce8027e6dcd4aa08d3994eaeadaa6e8
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-08 21:25:43 -0500

    added post and put functionality

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,21 +1,42 @@
 const express = require('express');
-const router = express.Router();
+let router = express.Router();
+const burger = require('../models/burger.js');
 
 router.get("/", function(req, res){
-
+    burger.all(function(data){
+        let hbars = {
+            burgers: data
+        };
+        console.log(hbars);
+        res.render('index', hbars);
+    })
 });
 
-router.post("/", function(req, res){
-
+router.post("/api/burgers", function(req, res){
+    burger.insert([
+        "burger_name"
+     ], [
+         req.body.burger_name
+     ], function(result){
+         res.json({ id: result.insertId})
+     });
 });
 
-router.put("/", function(req, res){
+router.put("/api/burgers/:id", function(req, res){
+    let condition = `id = ${req.params.id}`;
+    console.log('condition', condition);
 
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result){
+        if(result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+    });
 });
 
-router.delete("/", function(req, res){
-
-});
 
 
 module.exports = router;
