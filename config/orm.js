a2c3a10d930eebb588242efa1a32e7e0ba2d38d9
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-10 15:53:26 -0500

    added keyAndValue function, updated query strings.

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,28 @@
 let connection = require('../config/connection');
 
-function printQuestionMarks(num) {
-    var arr = [];
-  
-    for (var i = 0; i < num; i++) {
-      arr.push("?");
-    }
-  
-    return arr.toString();
-  }
-  
-  // Helper function to convert object key/value pairs to SQL syntax
-  function objToSql(ob) {
-    var arr = [];
-  
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
-        arr.push(key + "=" + value);
-      }
-    }
-  
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
-  }
+
+ function keyAndValue (obj) {
+   let combined = [];
+
+   for (let key in obj) {
+     let val = obj[key];
+        
+     let keyName = Object.keys(obj)[0];
+
+     if (obj[key] === 'true' || obj[key] === 'false'){
+        combined.push(keyName + "=" + val);
+     } else {
+        
+        let stringedKey = "'" + keyName + "'";
+
+        let stringedVal = "'" + val + "'";
+
+         combined.push(stringedKey + "=" + stringedVal);
+     }
+   }
+   return combined.toString();
+ }
+
 
 let orm = {
     selectAll: function(tableName, cb) {
@@ -42,38 +33,25 @@ let orm = {
         });
     },
     insertOne: function(tableName, cols, vals, cb) {
-        var queryString = "INSERT INTO " + tableName;
-
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
-
+        let queryString = `INSERT INTO ${tableName} (${cols.toString()}) VALUES (?,?);`;
 
         connection.query(queryString, vals, function(err, result){
             if (err) {
                 throw err;
             }
+            
             cb(result);
 
         });
     },
     updateOne: function(tableName, objColVals, condition, cb) {
-        var queryString = "UPDATE " + tableName;
-
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
-
-        console.log(queryString);
-        connection.query(queryString, function(err, result) {
+        let queryStringTwo = `Update ${tableName} SET ${keyAndValue(objColVals)} WHERE ${condition};`
+        
+        connection.query(queryStringTwo, function(err, result) {
         if (err) {
             throw err;
         }
-
+        
         cb(result);
         });
     }
