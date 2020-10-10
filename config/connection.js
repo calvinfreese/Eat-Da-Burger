dc3848b86c6c407923cf45bf390910a843698cc8
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-10 16:43:30 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-// const credentials = require('../config');
+const credentials = require('../config');
+let connection;
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "potato",
-  password: "1pass2word3",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(procexx.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: credentials.user,
+    password: credentials.password,
+    database: "burgers_db"
+  });
+  
+}
 
 // Make connection.
 connection.connect(function(err) {
