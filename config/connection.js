7a5cd296d382323a7fb57fc165f34b9919816830
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-10-10 16:34:30 -0500

    unquired credentials

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
-  user: "potato",
-  password: "1pass2word3",
+  user: "",
+  password: "",
   database: "burgers_db"
 });
 
